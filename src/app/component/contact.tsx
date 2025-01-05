2bc1d4d99cd1215f4341a5c54a15526d2fba68f1
adeenajaweid96 <adeenajaweid96@gmail.com>
2025-01-05 06:26:06 -0800

    Portfolio

--- src/app/component/contact.tsx
+++ src/app/component/contact.tsx
@@ -6,10 +6,10 @@ const Contact = () => {
     return (
         <div className='flex flex-wrap pt-20 justify-evenly py-4 border-b-2 border-b-gray-500 bg-[#121212] max-w-screen-2xl mx-auto'>
             <div className='py-20'>
-                <h1 className='text-white font-bold text-2xl'>Let's Connects</h1>
-                <p className='text-gray-500 text-[17px] '>I'm currently looking for new opportunities,You can connect<br/>
+                <h1 className='text-white font-bold text-2xl'>Lets Connects</h1>
+                <p className='text-gray-500 text-[17px] '>I am currently looking for new opportunities,You can connect<br/>
                  with me through github and linkedIn always open. Whether<br/>
-                  you have a question or just want to say hi, I'll try my best<br/>
+                  you have a question or just want to say hi, I will try my best<br/>
                       to get back to you!<br/></p>
                       <div className='flex  mx-4 '>
                      <a href="https://www.linkedin.com/in/adeenajaweid96/" target='_blank'> 
@@ -25,7 +25,7 @@ const Contact = () => {
 <label htmlFor="text" className='text-white pb-2'>Subject</label>
 <input type="text" placeholder='Just saying hi' className='bg-[#18191E] border-2 border-gray-800 w-[30rem] p-6 mb-6 h-[5%] rounded-md max-sm:w-[10rem]'/>
         <label htmlFor="Message" className='pb-2 text-white'>Message</label>
-        <textarea name="" className='bg-[#18191E] text-gray-400 border-2 border-gray-800 w-[30rem] p-6 mb-6 h-[20%] rounded-md max-sm:w-[10rem]'>Let's talk about..</textarea>
+        <textarea  className='bg-[#18191E] text-gray-400 border-2 border-gray-800 w-[30rem] p-6 mb-6 h-[20%] rounded-md max-sm:w-[10rem]'>Lets talk about..</textarea>
            <button className='bg-[#9333EA] text-white p-2 rounded-md'>Send Message</button>
             </div>
         </div>
--- src/app/component/hero.tsx
+++ src/app/component/hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className='flex  justify-center gap-6 flex-wrap md:flex-wrap bg-[#121212] max-w-screen-2xl mx-auto'>
           <div className='flex flex-col flex-wrap pt-32 '>
-<h1 className='md:text-6xl font-semibold  text-[#D4419D] max-sm:text-3xl'>Hi , I'm Adeena</h1>
+<h1 className='md:text-6xl font-semibold  text-[#D4419D] max-sm:text-3xl'>Hi , I am Adeena</h1>
 <h2 className='md:text-6xl font-semibold text-[#C478EA] pt-5 max-sm:text-3xl '>Frontend Developer</h2>
 <div>
 <button className=' text-xs text-white font-bold bg-[#D44190]  w-28 rounded-full  p-2 m-2 '>Hire Me</button>
